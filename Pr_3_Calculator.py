a4fa988a391bfbbfe1a544357c92a4f2a032c949
ShizNyan <Beelinebeebee@yandex.ru>
2022-03-01 19:22:30 +0300

    Законченный калькулятор

--- Pr_3_Calculator.py
+++ Pr_3_Calculator.py
@@ -1,7 +1,6 @@
 #Научный калькулятор(решение уравнений, матричные операции, интегрирование и т. д.).
+import math
 
-
-print("Hello there")
 
 class Test():
     
@@ -33,6 +32,74 @@ class Test():
         else:
             print("Тест не пройден")
 
+    def test_matrix_add(self, c):
+        print("testing matrix_add")
+        mx = [[1, 1],
+              [1, 1]]
+        my = [[1, 1],
+              [1, 1]]
+        mres = [[2, 2],
+                [2, 2]]
+        res = c.matrix_add(mx, my, 2)
+        if res == mres:
+            print("Тест пройден")
+        else:
+            print("Тест не пройден")
+
+    def test_matrix_sub(self, c):
+        print("testing matrix_sub")
+        mx = [[1, 1],
+              [1, 1]]
+        my = [[1, 1],
+              [1, 1]]
+        mres = [[0, 0],
+                [0, 0]]
+        res = c.matrix_sub(mx, my, 2)
+        if res == mres:
+            print("Тест пройден")
+        else:
+            print("Тест не пройден")
+
+    def test_matrix_mul(self, c):
+        print("testing matrix_mul")
+        mx = [[1, 1],
+              [1, 1]]
+        my = [[2, 3],
+              [2, 3]]
+        mres = [[4, 6],
+                [4, 6]]
+        res = c.matrix_mul(mx, my, 2)
+        if res == mres:
+            print("Тест пройден")
+        else:
+            print("Тест не пройден")
+
+    def test_matrix_transpose(self, c):
+        print("testing matrix_transpose")
+        mx = [[1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4],
+            [1, 2, 3, 4]]
+        mres = [[1, 1, 1, 1],
+                [2, 2, 2, 2],
+                [3, 3, 3, 3],
+                [4, 4, 4, 4]]
+        res = c.matrix_transpose(mx, 4)
+        if res == mres:
+            print("Тест пройден")
+        else:
+            print("Тест не пройден")
+
+    def test_integrate(self, c):
+        print("testing integrate")
+        tres = 0.4221669591570937
+        tres_r = -0.0003028591589098281
+        res, res_r = c.integrate(c, 0, 1, 5)
+        if res == tres and tres_r == res_r:
+            print("Тест пройден")
+        else:
+            print("Тест не пройден")
+
 
 class Calculator():
 
@@ -51,23 +118,78 @@ class Calculator():
         else:
             return "N/A"
 
-    def matrix_add(self, x, y):
+    def matrix_add(self, x, y, size):
+        for i in range(size):
+            for j in range(size):
+                x[i][j] += y[i][j]
+        return x
+
+    def matrix_sub(self, x, y, size):
+        for i in range(size):
+            for j in range(size):
+                x[i][j] -= y[i][j]
+        return x
+
+    def matrix_mul(self, x, y, size):
+        res = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+        t =0
         
-        print("+")
+        for i in range(size):
+            for j in range(size):
+                t += x[i][j]*y[j][i]
+            res[i][j] = t
+            t = 0
+        return res
 
-    def matrix_sub(self, x, y):
-        print("-")
+    def matrix_transpose(self, x, size):
+        res = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+        for i in range(size):
+            for j in range(size):
+                res[j][i] = x[i][j]
+        return res
 
-    def matrix_mul(self, x, y):
-        print("*")
+    def f(self, x): #функция интеграла
+        return 1 / (1 + math.exp(x))
 
-    def matrix_transpose(self, x, y):
-        print("T")
+    def f_2(self, x):
+        return (-1 + (2 * (math.exp(x) / (math.exp(x) + 1)))) * math.exp(x) / (pow((1 + math.exp(x)), 2))
 
-    def matrix_reverse(self, x, y):
-        print("R")
+    def integrate(self, c, a, b, k):
 
-    def integrate(self, x, y):
+        x = [i for i in range(k)]
+        k2 = k + 1
+
+        I = 0 #решение интеграла
+        I1 = 0 #первая часть интеграла
+        I2 = 0 #вторая часть интеграла
+        R = 0 #остаточный член
+        h = 0 #шаг интегрирования
+        max = 0 #максимальное значение второй производной функции на интервале
+        h = (b - a) / k
+
+        for i in range(k2-1):
+            x[i] = c.f(h * i)
+
+        I1 = (b - a) / k
+        I2 = (x[0] + x[k-1]) / 2
+
+        for i in range(k-1):
+            I2 += x[i]
+        
+
+        max = c.f_2(b)
+
+        R = (-(b - a) / 12) * pow(h, 2) * max
+
+        I = I1 * I2
+    
+        return I, R
         print("Integral")
 
 
@@ -85,14 +207,12 @@ while (run):
     print("2 - Вычитание")
     print("3 - Умножение")
     print("4 - Деление")
-    print("5 - Решение уравнения??")
-    print("6 - Сложение матриц")
-    print("7 - Вычитание матриц")
-    print("8 - Умножение матриц")
-    print("9 - Транспонирование матриц")
-    print("10 - Нахождение обратно матрицы")
-    print("11 - Интегрирование")
-    print("12 - Закончить работу")
+    print("5 - Сложение матриц (4х4)")
+    print("6 - Вычитание матриц (4х4)")
+    print("7 - Умножение матриц (4х4)")
+    print("8 - Транспонирование матрицы (4х4)")
+    print("9 - Интегрирование по частям")
+    print("10 - Закончить работу")
     m = int(input())
 
     if m == 1:
@@ -134,13 +254,59 @@ while (run):
             print("N/A")
         t.test_div(c)
 
+    elif m == 5:
+        matrix_x = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+        matrix_y = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+
+        size = 4
+        print("Введите матрицу x")
+        for i in range (size):
+            print("Введите строку "+str(i+1))
+            for j in range (size):
+                k = int(input())
+                matrix_x[i][j] = k
+        print()
+        print("Матрица x:")
+        for i in range (size):
+            print(matrix_x[i])
+        print()
+
+        print("Введите матрицу y")
+        for i in range (size):
+            print("Введите строку "+str(i+1))
+            for j in range (size):
+                k = int(input())
+                matrix_y[i][j] = k
+        print()
+        print("Матрица y:")
+        for i in range (size):
+            print(matrix_y[i])
+
+        print()
+
+        res = c.matrix_add(matrix_x, matrix_y, size)
+        print("Результат сложения матриц:")
+        for i in range (size):
+            print(res[i])
+        t.test_matrix_add(c)
+
     elif m == 6:
-        matrix_x = []
-        matrix_y = []
+        matrix_x = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+        matrix_y = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
 
-        print("Введите размерность матриц")
-        size = int(input())
-        print()
+        size = 4
         print("Введите матрицу x")
         for i in range (size):
             print("Введите строку "+str(i+1))
@@ -149,10 +315,39 @@ while (run):
                 matrix_x[i][j] = k
         print()
         print("Матрица x:")
-        for i in range (4):
+        for i in range (size):
             print(matrix_x[i])
         print()
 
+        print("Введите матрицу y")
+        for i in range (size):
+            print("Введите строку "+str(i+1))
+            for j in range (size):
+                k = int(input())
+                matrix_y[i][j] = k
+        print()
+        print("Матрица y:")
+        for i in range (size):
+            print(matrix_y[i])
+        print()
+
+        res = c.matrix_sub(matrix_x, matrix_y, size)
+        print("Результат вычитания матриц:")
+        for i in range (size):
+            print(res[i])
+        t.test_matrix_sub(c)
+
+    elif m == 7:
+        matrix_x = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+        matrix_y = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+
+        size = 4
         print("Введите матрицу x")
         for i in range (size):
             print("Введите строку "+str(i+1))
@@ -161,20 +356,64 @@ while (run):
                 matrix_x[i][j] = k
         print()
         print("Матрица x:")
-        for i in range (4):
+        for i in range (size):
             print(matrix_x[i])
-
         print()
 
-        res = c.matrix_add(matrix_x, matrix_y)
-        print("Результат деления:")
-        if res != "N/A":
-            print(res)
-        else:
-            print("N/A")
-        t.test_div(c)
+        print("Введите матрицу y")
+        for i in range (size):
+            print("Введите строку "+str(i+1))
+            for j in range (size):
+                k = int(input())
+                matrix_y[i][j] = k
+        print()
+        print("Матрица y:")
+        for i in range (size):
+            print(matrix_y[i])
+        print()
 
-    elif m == 12:
+        res = c.matrix_mul(matrix_x, matrix_y, size)
+        print("Результат умножения матриц:")
+        for i in range (size):
+            print(res[i])
+        t.test_matrix_mul(c)
+
+    elif m == 8:
+        matrix_x = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+
+        size = 4
+        print("Введите матрицу x")
+        for i in range (size):
+            print("Введите строку "+str(i+1))
+            for j in range (size):
+                k = int(input())
+                matrix_x[i][j] = k
+        print()
+        print("Матрица x:")
+        for i in range (size):
+            print(matrix_x[i])
+        print()
+
+        res = c.matrix_transpose(matrix_x, size)
+        print("Результат транспонирования матрицы:")
+        for i in range (size):
+            print(res[i])
+        t.test_matrix_transpose(c)
+
+    elif m == 9:
+        a = 0 #пределы интегрирования
+        b = 1
+        print("Введите количество интервалов разбиения")
+        k = int(input())
+        res, res_r = c.integrate(c, a, b, k)
+        print("Результат интегрирования по частям:")
+        print(res,"+-",res_r)
+        t.test_integrate(c)
+
+    elif m == 10:
         print("Закончить работу")
         run = False
 
